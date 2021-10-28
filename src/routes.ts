27821221e86bdf707cb00124d729c950fcb4437c
JosielMatos <josielmatos77@protonmail.com>
2021-10-28 12:28:15 -0300

    3 last messages route

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
 import { CreateMessageController } from "./controllers/CreateMessageController";
+import { GetLast3MessagesController } from "./controllers/GetLast3MessagesController";
 import { ensureAuth } from "./middlewares/ensureAuth";
 
 const router = Router();
@@ -8,5 +9,8 @@ const router = Router();
 //post routes
 router.post("/auth", new AuthenticateUserController().handle)
 router.post("/messages", ensureAuth, new CreateMessageController().handle)
+
+//get last 3 messages
+router.get("/messages/last3", new GetLast3MessagesController().execute)
 
 export { router };
