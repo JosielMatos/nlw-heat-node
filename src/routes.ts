024a2d860c788ed16534c31d6716efeba1529f99
JosielMatos <josielmatos77@protonmail.com>
2021-10-28 12:47:04 -0300

    user profile data route

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
 import { CreateMessageController } from "./controllers/CreateMessageController";
 import { GetLast3MessagesController } from "./controllers/GetLast3MessagesController";
+import { UserProfileController } from "./controllers/UserProfileController";
 import { ensureAuth } from "./middlewares/ensureAuth";
 
 const router = Router();
@@ -10,7 +11,8 @@ const router = Router();
 router.post("/auth", new AuthenticateUserController().handle)
 router.post("/messages", ensureAuth, new CreateMessageController().handle)
 
-//get last 3 messages
+//get routes
 router.get("/messages/last3", new GetLast3MessagesController().execute)
+router.get("/profile", ensureAuth, new UserProfileController().handle)
 
 export { router };
